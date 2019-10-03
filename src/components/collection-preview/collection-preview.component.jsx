da2e09b31f77ea1e921ef60b96a6a7362d1887bf
John <john@pop.corn>
2019-10-04 01:23:32 +0300

    Converted to css in js. Implement code that pulls in shop data from firestore and stores it into the shop reducer from our shop page. Also included WithSponner HOC

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component';
-import './collection-preview.styles.scss';
+import { 
+  CollectionPreviewContainer, 
+  TitleContainer, 
+  PreviewContainer 
+} from './collection-preview.styles';
 
 const CollectionPreview = ({ title, items }) => {
   return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview">
+    <CollectionPreviewContainer>
+      <TitleContainer>{title.toUpperCase()}</TitleContainer>
+      <PreviewContainer>
         {
           items
           .filter((item, index) => index < 4)
@@ -15,8 +19,8 @@ const CollectionPreview = ({ title, items }) => {
             <CollectionItem key={item.id} item={item} />
           ))
         }
-      </div>
-    </div>
+      </PreviewContainer>
+    </CollectionPreviewContainer>
   );
 };
 
